af757d4f01e17ca9cd73f16015a032a65ed2acaf
lunaracres8-lgtm <lunaracres8@gmail.com>
2026-09-24 22:33:45 -0400

    Fix Baker's Dawgs Supabase project URL typo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BD_CONFIG = {
-  SUPABASE_URL: "https://dmxgsnpkncvueurjsmvk.supabase.co",
+  SUPABASE_URL: "https://dmxqsnpkncvueurjsmvk.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_JNEuiF5iHxBhzrED25Zftw_U0KasMfn"
 };
